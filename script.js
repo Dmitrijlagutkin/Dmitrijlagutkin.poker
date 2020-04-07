d3dc0da616df11855d256c515b61cfe0c1b2f84d
Dmitrij Lagutkin <dmitrijlagutkin@gmail.com>
2020-04-07 21:03:45 +0200

    chenges

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let dice = document.querySelector(".dice")
 let playerOne = document.querySelector(".player_one")
 let playerTwo = document.querySelector(".player_two")
 //--------Default value for playerTwo----------
-playerTwo.classList.add("playerOff")
+playerTwo.classList.add("Off")
 
 let pointDiceArr = []
-let counterDdropDices = 0
+let counterDdropDices = null
 
 function getInnerHtmlArr(domElement, targetArr) {
 	for (let element of domElement) {
@@ -16,44 +16,68 @@ function getInnerHtmlArr(domElement, targetArr) {
 	}
 }
 
-function dropDices(domElement, targetArr) {
-	btnDice.addEventListener("click", event => {
-		counterDdropDices++
-		let defaultNum = null
-		for (let i = 0; i < domElement.length; i++) {
+
+btnDice.addEventListener("click", event => {
+	counterDdropDices++
+
+	let defaultNum = null
+	if (counterDdropDices < 4) {
+		for (let i = 0; i < dicesList.length; i++) {
 			defaultNum = Math.trunc((Math.random() * 6))
-			if (!domElement[i].classList.contains("dice_active")) {
-				domElement[i].innerHTML = targetArr[defaultNum]
+			if (!dicesList[i].classList.contains("dice_active")) {
+				dicesList[i].innerHTML = pointDiceArr[defaultNum]
 			}
 		}
-	})
+		if (counterDdropDices === 3) {
+			btnDice.innerHTML = "Выберите комбинацию"
+			chooseСombination()
+		}
+	} else if (counterDdropDices === 4) {
+		btnDice.innerHTML = "Завершить ход?"
+	} else {
+		chengePlayer()
+		counterDdropDices = 0
+		btnDice.innerHTML = "Бросить кубики"
+		removeClassActive(dicesList)
+		for (let i = 0; i < dicesList.length; i++) {
+			dicesList[i].innerHTML = pointDiceArr[i]
+		}
+	}
+})
+
+function removeClassActive(elem) {
+	for (let i = 0; i < elem.length; i++) {
+		elem[i].classList.remove("dice_active")
+		for (let j = 0; j < elem[i].children.length; j++) {
+			elem[i].children[j].classList.remove("dice_point_active")
+		}
+	}
 }
 
 function chooseDices() {
 	dice.addEventListener("click", event => {
-		event.target.classList.add("dice_active")
+		event.target.classList.toggle("dice_active")
 		for (let i = 0; i < event.target.children.length; i++) {
-			event.target.children[i].classList.add("dice_point_active")
+			event.target.children[i].classList.toggle("dice_point_active")
 		}
 	})
+
 }
 
 // ----------------смена игрока-------------------
 
-function chengePlayer(counter) {
-	if (counter % 3 === 0) {
-		playerOne.classList.add("Off")
-		playerTwo.classList.remove("Off")
-	} else {
-		playerTwo.classList.add("Off")
-		playerOne.classList.remove("Off")
-	}
+function chengePlayer() {
+	playerOne.classList.toggle("Off")
+	playerTwo.classList.toggle("Off")
+}
+
+function chooseСombination() {
+	console.log("Выбор комбинации")
 }
 
 function taskManager() {
-	chengePlayer(counterDdropDices)
+
 	getInnerHtmlArr(dicesList, pointDiceArr)
-	dropDices(dicesList, pointDiceArr)
 	chooseDices()
 }
 
